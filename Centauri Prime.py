5a29eb7b017c40a62d704c76bc8a0232547798c1
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2022-08-16 21:44:51 +0530

    Create Centauri Prime.py

---
+++ Centauri Prime.py
@@ -0,0 +1,18 @@
+def isvowel(a):
+    if a in ('a','e','i','o','u','A','E','I','O','U'):
+        return True
+
+def getRuler(x):
+    last_letter = x[-1]
+    if isvowel(last_letter):
+        return "Alice"
+    if ( last_letter == "y" or last_letter == "Y" ):
+        return "Nobody"
+    return "Bob"
+
+
+T =input()
+for i in range(int(T)):
+    kingdom = input()
+    print("Case #{}: {} is ruled by {}".format(i+1, kingdom, getRuler(kingdom)))
+
