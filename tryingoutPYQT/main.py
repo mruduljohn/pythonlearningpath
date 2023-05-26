ccf3b6157c6a59b8a7bd554f707420782254f4b2
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-05-26 21:05:58 +0530

    Update main.py

--- tryingoutPYQT/main.py
+++ tryingoutPYQT/main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
+from PyQt5.QtCore import QObject, pyqtSlot
 
 # Create the application
 app = QGuiApplication(sys.argv)
@@ -9,11 +10,21 @@ app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
 
 # Load the QML file
-engine.load('path/to/your/qml/file.qml')
+engine.load('C:/Users/mrudu/Documents/GitHub/pythonlearningpath/tryingoutPYQT/Course_Success_.ui.qml')
 
 # Check if loading was successful
 if not engine.rootObjects():
     sys.exit(-1)
+class BackendObject(QObject):
+    @pyqtSlot(str)
+    def greet(self, name):
+        print('Hello,', name)
+
+# Create an instance of the backend object
+backend = BackendObject()
+
+# Expose the backend object to QML
+engine.rootContext().setContextProperty('backend', backend)
 
 # Run the application
 sys.exit(app.exec())
