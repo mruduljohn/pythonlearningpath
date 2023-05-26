bb4f362b9463badaf0a0c31f0f5b68250366b9f1
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-05-26 22:01:59 +0530

    Update main.py

--- tryingoutPYQT/main.py
+++ tryingoutPYQT/main.py
@@ -3,13 +3,6 @@ from PyQt5.QtCore import QObject,QUrl
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQuick import QQuickView
 
-class Constants(QObject):
-    def __init__(self):
-        super().__init__()
-        self.width = 400
-        self.height = 300
-        self.backgroundColor = "#ffffff"
-        self.font = "Arial"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
